63ec73b66f6194b504973fcf2657f2f47e150aef
Chris Bowdon <c.bowdon@bath.edu>
2014-06-30 22:05:10 +0100

    Implemented addNewEntryRow.

---
+++ montgomery.ts
@@ -0,0 +1,1 @@
+/// <reference path="view-controller.ts" />
--- view-controller.ts
+++ view-controller.ts
@@ -1,7 +1,31 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
 
-module ViewController {
-    export function addNewEntryRow() {
-        throw new Error('nope');
+class ViewController {
+
+    constructor() {
+        // TODO addNewEntryRow should actually be trigged by store publish
+        // and the button click should push to store
+        $('div.current-row > button#add').click(el => this.addNewEntryRow(el));
+    }
+
+    addNewEntryRow(evt: Event) {
+        var $row, rowId, $clone;
+
+        $row = $(evt.target).parent('.row-container.current-row');
+        rowId = $row.attr('id');
+
+        $clone = $row.clone().attr('id', rowId + 'x');
+
+        $clone.insertAfter($row);
+
+        $row.find('button#add').hide().off('click');
+        $row.removeClass('current-row');
+        $clone.find('button#add').click(el => this.addNewEntryRow(el));
+    }
+
+    updateSum() {
+        throw new Error('nein');
     }
 }
+
+var AppViewController = new ViewController();

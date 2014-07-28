6c1a617489d18138b129e04b4bd95800d7a4d31e
Chris Bowdon <c.bowdon@bath.edu>
2014-07-28 21:09:12 +0100

    Time parses tests.

--- test/time.ts
+++ test/time.ts
@@ -5,26 +5,26 @@ module Test {
 
     QUnit.test('Parse string: HH:mm', assert => {
         var time = Time.parse('23:45');
-        assert.strictEqual(time.minute, 45);
-        assert.strictEqual(time.hour, 23);
+        assert.strictEqual(time.minute, 45, 'minute');
+        assert.strictEqual(time.hour, 23, 'hour');
     });
 
     QUnit.test('Parse string: HHmm', assert => {
         var time = Time.parse('2345');
-        assert.strictEqual(time.minute, 45);
-        assert.strictEqual(time.hour, 23);
+        assert.strictEqual(time.minute, 45, 'minute');
+        assert.strictEqual(time.hour, 23, 'hour');
     });
 
     QUnit.test('Parse string: H:mm', assert => {
         var time = Time.parse('9:45');
-        assert.strictEqual(time.minute, 45);
-        assert.strictEqual(time.hour, 9);
+        assert.strictEqual(time.minute, 45, 'minute');
+        assert.strictEqual(time.hour, 9, 'hour');
     });
 
     QUnit.test('Parse string: Hmm', assert => {
         var time = Time.parse('945');
-        assert.strictEqual(time.minute, 45);
-        assert.strictEqual(time.hour, 9);
+        assert.strictEqual(time.minute, 45, 'minute');
+        assert.strictEqual(time.hour, 9, 'hour');
     });
 
     QUnit.test('Parse string: time out of range', assert => {
--- time.ts
+++ time.ts
@@ -1,7 +1,17 @@
 class Time {
 
     static parse(str: string) : Time {
-        throw new Error('not yet implemented');
+        var regex = /^([0-9]|[0-1][0-9]|2[0-3]):?([0-5][0-9])$/,
+            match = regex.exec(str),
+            hour: number,
+            minute: number;
+
+        if (match) {
+            hour = parseInt(match[1], 10);
+            minute = parseInt(match[2], 10);
+            return new Time(hour, minute);
+        }
+        throw new TypeError('Cannot parse time: ' + str);
     }
 
     constructor(public hour: number, public minute: number) {}

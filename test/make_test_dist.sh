c6f31699a92520aeabb79a265dd7fb9bab76b999
Chris Bowdon <c.bowdon@bath.edu>
2014-09-05 08:34:53 +0800

    Added blanket code coverage.

--- test/make_test_dist.sh
+++ test/make_test_dist.sh
@@ -6,6 +6,7 @@ cp  bin/montgomery.js \
     bower_components/moment/moment.js \
     bower_components/qunit/qunit/qunit.js \
     bower_components/qunit/qunit/qunit.css \
+    bower_components/blanket/dist/qunit/blanket.js \
     test/bin/montgomery-test.js \
     test/index.html \
     test/dist
